f62715d86bc6521a53ac549ff6313a99378c786c
Mikhli, Uri <urimikhli@gmail.com>
2015-03-16 21:40:42 -0400

    Working importer. Needs a little cleanup, but otherwise functional

---
+++ importer.rb
@@ -0,0 +1,58 @@
+require 'csv'
+
+class Importer
+  attr_accessor :records
+
+  #need to instantiate the class because we want an instance to only be able to work on one file at atime.
+  def initialize (filename = nil)
+    return " Missing filename to import" if filename.empty?
+    import(filename)
+  end 
+
+  def formats
+    { csv: :import_CSV_file }
+  end
+
+  private
+
+  def import(filename)
+    #check the file extension and return the file format to use
+    file_format = validate_extension(filename)
+
+    #check the model, default to stock_item
+    if validate_format?(file_format)
+      #use the file format
+      import_method = formats[file_format.to_sym]
+      self.method(import_method.to_s).call(filename) 
+    end
+  end
+
+  def validate_extension(filename)
+    #this is where new file type formats are added.
+    if /\.csv$/.match(filename)
+      "csv"
+    else
+      raise "Unknown file extension. Please try another extension or add it to this list:
+      *.csv
+      "
+    end
+  end
+
+  def validate_format?(file_format)
+    valid_formats = formats.keys
+    return formats.keys.include? file_format.to_sym
+  end
+
+  def import_CSV_file(filename)
+    @records = []
+    raise "no file found " unless File.exist?(filename)
+    CSV.foreach(filename, :headers => true) do |row|
+      @records.push row
+    end
+  end
+end
+
+def importfile(filename)
+  importer = Importer.new(filename)
+  importer.records
+end
